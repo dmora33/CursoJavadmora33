990abc697845df9e4403cb1e90634eed68a099b4
ClaseJavaGetafe <Tardes@GET-04>
2025-07-09 20:49:01 +0200

    solucion para hacer eje4

--- src/es/cursojava/inicio2/string/StringEjercicio4PalabraMasLarga.java
+++ src/es/cursojava/inicio2/string/StringEjercicio4PalabraMasLarga.java
@@ -5,11 +5,8 @@ public class StringEjercicio4PalabraMasLarga {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String cadena = "esto es una cadena";
-		for (int i =0; i<=cadena.length()-1;i++) {
-			if (cadena.charAt(i)) {
-				
-			}
-		}
+		//usar index of!!!!!
+		//y luego substring
 
 	}
 
