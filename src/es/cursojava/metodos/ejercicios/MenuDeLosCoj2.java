df8a26aae61fad1eb682fc977a55dbb824ebda9c
usuario <usuario@DESKTOP-ASP8527.mshome.net>
2025-07-28 07:41:17 +0200

    solo falta depurar el menu de los cojosn2

--- src/es/cursojava/metodos/ejercicios/MenuDeLosCoj2.java
+++ src/es/cursojava/metodos/ejercicios/MenuDeLosCoj2.java
@@ -24,19 +24,72 @@ public class MenuDeLosCoj2 {
 
 	}
 
+	public static void validarMail(String email) {
+
+		String mensajeError = "";
+
+		email = email.trim();
+
+		if (email.isEmpty()) {
+			mensajeError = "El email no puede estar en blanco";
+		} else {
+			if (email.contains(" ")) {
+				mensajeError = "El email no puede tener espacios en blanco\n";
+			}
+
+			if (!email.contains("@")) {
+				mensajeError += "El email no tiene arroba\n";
+			} else if (email.indexOf("@") != email.lastIndexOf("@")) {
+				mensajeError += "El email no puede tener más de 1 arroba\n";
+			} else {
+				String dominio = email.substring(email.indexOf("@") + 1);
+				System.out.println("dominio: " + dominio);
+
+				if (!dominio.contains(".")) {
+					mensajeError += "El dominio del email debe contener al menos un punto \n";
+				} else {// Si contiene punto
+					int posicionPunto = dominio.indexOf(".");
+					if (posicionPunto < 2) {
+						mensajeError += "La separación entre la @ y el primer punto debe ser superior a 2 caracteres \n";
+					}
+
+					int posicionUltimoPunto = dominio.lastIndexOf(".");
+					int longitudDominio = dominio.length() - 1;
+					if (longitudDominio - posicionUltimoPunto < 2 || longitudDominio - posicionUltimoPunto > 6) {
+						mensajeError += "Después del último punto solo puede haber entre 2 y 6 caracteres \n";
+					}
+				}
+
+			}
+
+		}
+
+		if (mensajeError.isEmpty()) {
+			System.out.println("El email " + email + " es valido");
+		} else {
+			System.out.println(mensajeError);
+		}
+
+	}
+
 	public static void main(String[] args) {
+		String colegio[][] = new String[2][3];
+
+		int opcion = 0;
+
+		// entrada y salda del menu
+		colegio[0][1] = "Daniel";
+		colegio[1][2] = "Juan";
+
 		// TODO Auto-generated method stub
 
 		String[] opcionesMenu = { "\nMenu", "1. Pintar Cuadro", "2. Validar email", "3. Añadir Alumno", "4. Salir" };
-
-		int opcion;
+		Scanner scan = new Scanner(System.in);
 
 		do {
 			PintaMenuYTexto.pintaMenu(opcionesMenu);
-		System.out.println("Escoge una opción");
+			System.out.println("Escoge una opción");
 
-
-			Scanner scan = new Scanner(System.in);
 			opcion = scan.nextInt();
 
 			switch (opcion) {
@@ -49,12 +102,41 @@ public class MenuDeLosCoj2 {
 
 			case 2:
 
-				System.out.println("2. Validar email");
+				System.out.println("2. Validar email \nIntroduce el mail");
+				scan = new Scanner(System.in);
+				String mail = scan.nextLine();
+				validarMail(mail);
 
 				break;
 
-			case 3:
+			case 3: {
 				System.out.println("3. Añadir Alumno");
+				boolean hayHueco = false;
+				for (int i = 0; i < colegio.length; i++) {
+					for (int j = 0; j < colegio[i].length; j++) {
+						if (colegio[i][j] == null) {
+							scan = new Scanner(System.in);
+							System.out.println(" | Aula: " + i + " | puesto" + j);
+							System.out.println("Añadir nombre del Alumno:");
+							String nombre = scan.nextLine();
+							// si pulsamos enter sin introducir datos y quitamos los espacios en blanco
+							// salto a otro espacio
+							if (nombre.trim().isEmpty()) {
+								System.out.println("Campo vacio pasamos al siguente vacio");
+							} else {
+								colegio[i][j] = nombre;
+								hayHueco = true;
+								System.out.println("Has añadido a" + nombre + " | Aula: " + i + " | puesto" + j);
+
+							}
+						}
+
+					}
+				}
+				if (!hayHueco) {
+					System.out.println("no hay huecos libre");
+				}
+			}
 				break;
 
 			case 4:
