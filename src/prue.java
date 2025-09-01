21a8054ab2bc8a6bf3c656ae909a0d39fc3126a7
ClaseJavaGetafe <Tardes@GET-04>
2025-09-01 16:21:21 +0200

    no se.. no me acuerdp

---
+++ src/prue.java
@@ -0,0 +1,20 @@
+
+public class prue {
+
+	public static void main(String[] args) {
+		
+			for (int i=1; i<=1000; i++) {
+			if (i % 2 == 0)
+			continue;
+			if (i % 3 == 0)
+			continue;
+			System.out.println("Números: " + i);
+			if (i >= 15)
+			break;
+			}
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/prueba.java
+++ src/prueba.java
@@ -2,12 +2,13 @@
 public class prueba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int x;
-        if (true) {
-            x = 10;
-        }
-        System.out.println(x);
+		
+			
+			String s = "Hola";
+			s.concat(" Mundo");
+			System.out.println(s);
+		
+		
 	}
 
 }
