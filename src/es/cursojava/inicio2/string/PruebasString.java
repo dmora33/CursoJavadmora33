1fd3e6ba042645ab1bf384db326941127e97226f
usuario <usuario@DESKTOP-ASP8527.mshome.net>
2025-09-10 10:30:46 +0200

    pruebas String

---
+++ src/es/cursojava/inicio2/string/PruebasString.java
@@ -0,0 +1,12 @@
+package es.cursojava.inicio2.string;
+
+public class PruebasString {
+
+	public static void main(String[] args) {
+String cadena = " la concha de tu madre ";
+System.out.println(cadena);
+System.out.println(cadena.replace(" ",""));
+System.out.println(cadena.charAt(cadena.length()-1));
+	}
+
+}
