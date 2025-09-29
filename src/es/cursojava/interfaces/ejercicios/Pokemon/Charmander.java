b2ae1dab4d20cc4793913dfcc264fb9ef05845a9
arom <arom@arom-ThinkPad-T460s>
2025-09-29 10:42:48 +0200

    pokemon charma

--- src/es/cursojava/interfaces/ejercicios/Pokemon/Charmander.java
+++ src/es/cursojava/interfaces/ejercicios/Pokemon/Charmander.java
@@ -1,5 +1,37 @@
 package es.cursojava.interfaces.ejercicios.Pokemon;
 
-public class Charmander {
+public class Charmander extends Pokemon implements IFuego {
 
+	public Charmander() {
+	}
+
+	@Override
+	protected void atacarPlacaje() {
+		System.out.println("Hola soy Charmander y este es mi ataque placaje");
+	}
+
+	@Override
+	protected void atacarAraniazo() {
+		System.out.println("Hola soy Charmander y este es mi ataque arañazo");
+	}
+
+	@Override
+	protected void atacarMordisco() {
+		System.out.println("Hola soy Charmander y este es mi ataque mordisco");
+	}
+
+	@Override
+	public void atacarPunioFuego() {
+		System.out.println("Hola soy Charmander y este es mi ataque puño fuego");
+	}
+
+	@Override
+	public void atacarLanzallamas() {
+		System.out.println("Hola soy Charmander y este es mi ataque lanzallamas");
+	}
+
+	@Override
+	public void atacarAscuas() {
+		System.out.println("Hola soy Charmander y este es mi ataque ascuas");
+	}
 }
