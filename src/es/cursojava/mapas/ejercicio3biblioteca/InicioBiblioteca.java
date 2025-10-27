31472d376175cc078ae29fcf68111895390897b3
ClaseJavaGetafe <Tardes@GET-04>
2025-10-27 20:20:12 +0100

    biblioteca incompleta copiar del prof

--- src/es/cursojava/mapas/ejercicio3biblioteca/InicioBiblioteca.java
+++ src/es/cursojava/mapas/ejercicio3biblioteca/InicioBiblioteca.java
@@ -3,32 +3,82 @@ package es.cursojava.mapas.ejercicio3biblioteca;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import es.cursojava.utils.Utilidades;
 
 public class InicioBiblioteca {
-	
-	
-	//TODO:Solicitar los datos de un libro, autor, titulo, isbn y año de publicación, 
+
+	// TODO:Solicitar los datos de un libro, autor, titulo, isbn y año de
+	// publicación,
 //	- si el autor no está en la biblioteca, solicitar la nacionalidad del autor y meterlo en la biblioteca, 
 //	- si el autor sí está ya, agregar el libro a su listado
-	
-	
+
 	private static void librosPubclicadosAnio(int anio, Map<Autor, List<Libro>> libreria) {
-		
+		Collection<List<Libro>> listaLibros = libreria.values(); // sacamos los libros!!!!!!!
+		for (List<Libro> list : listaLibros) {
+			for (Libro libro : list) {
+				if (libro.getAnio() > anio) {
+					System.out.println("libros del anio " + anio + libro.getTitulo());
+				}
+			}
+		}
 	}
-	
-	
+
 	private static void nacionalidadAutor(Map<Autor, List<Libro>> libreria) {
-		
+		Set<Autor> autores = libreria.keySet(); // sacamos con KeySet solo las Ket
+		for (Autor autor : autores) {
+			System.out.println(autor.getNacionalidad());
+			// solo saca las tres primeras letras
+			// como tengo un autor con nacional null
+//			try {
+//				System.out.println(autor.getNacionalidad().substring(0, 3));
+//			} catch (NullPointerException npe) {
+//				// npe significa que clase NullPointerException y la variable npe
+//				//
+//				System.out.println("error al optener la nacionalidad");
+//			} catch (StringIndexOutOfBoundsException siobe) {
+//				// npe significa que clase StringIndexOutOfBoundsException y la variable siobe
+//				//
+//				System.out.println("te has pasado de listo con las posiciones de la nacionalidad");
+//			} catch (Exception todasLasExcepciones) {
+//				System.out.println("error general, cualquiera... lo buscas, los coge todos");
+//			} finally {			System.out.println("todo lo que haya en finally se ejecuta siempre haya o no haya errores!!!!");
+//
+//			}
+
+		}
+
+	}
+	// + mostrar por cada autor que tenga más de dos libros su nombre y el nombre de
+	// todos los libros que tiene publicados
+
+	private static void autorMasDeDosLibros(Map<Autor, List<Libro>> libreria) {
+
+		Set<Entry<Autor, List<Libro>>> entrylibreria = libreria.entrySet(); // entry guarda las referencias
+
+		for (Entry<Autor, List<Libro>> entry : entrylibreria) {
+			Autor a = entry.getKey();
+
+			List<Libro> libros = entry.getValue();
+			if (libros.size() > 2) {
+				for (Libro libro : libros) {
+					System.out.println(libro.getTitulo());
+				}
+			}
+		}
 	}
 
 	private static void eliminarLibroIsbn(String isbn, Map<Autor, List<Libro>> libreria) {
 
 	}
+	
+	
 
 	public static void main(String[] args) {
 
@@ -57,12 +107,21 @@ public class InicioBiblioteca {
 		Libro libro8 = new Libro("Orgullo y prejuicio", 2009, "978-84-376-0497-8");
 
 		Autor autor1 = new Autor("Carlitos", "Espanol");
-		Autor autor2 = new Autor("Pepa", "Espanol");
+		Autor autor2 = new Autor("Pepa", null); // cambio a null para que me de un error y tener que hacer el control de
+												// errores
 		Autor autor3 = new Autor("Pepito", "Espanol");
 		Autor autor4 = new Autor("Menganito", "Espanol");
 
-		List<Libro> listaLibrosAutor1 = Arrays.asList(libro0, libro1, libro2);
-		List<Libro> listaLibrosAutor2 = Arrays.asList(libro3, libro4);
+		// si queremos modificar la lista hay que hacerlo asi no nos vale Arrays.asList
+		// List<Libro> listaLibroAutor = new ArrayList();
+
+		List<Libro> listaLibrosAutor1 = new ArrayList();
+		listaLibrosAutor1.add(libro0);
+		listaLibrosAutor1.add(libro1);
+		// listaLibrosAutor1.add(libro2);
+
+		List<Libro> listaLibrosAutor2 = Arrays.asList(libro3, libro4);// con arrys.aslist no podemos modificar el
+																		// contenido
 		List<Libro> listaLibrosAutor3 = Arrays.asList(libro5, libro6);
 		List<Libro> listaLibrosAutor4 = Arrays.asList(libro7, libro8);
 
@@ -71,8 +130,10 @@ public class InicioBiblioteca {
 		libreria.put(autor2, listaLibrosAutor2);
 		libreria.put(autor3, listaLibrosAutor3);
 		libreria.put(autor4, listaLibrosAutor4);
+	//	libreria.get(listaLibrosAutor1).add(libro2); // tambien podemos añadir libro al autor desde la bibliotenca, esto est mal!!!!!
 
+		nacionalidadAutor(libreria); 
+		autorMasDeDosLibros(libreria);
 	}
-	
 
 }
