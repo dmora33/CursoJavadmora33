82bad0939da821a21ad960e0cc8ae9d063ad57c6
Isabel Eclipse <Tardes@GET-09>
2025-11-07 16:28:27 +0100

    arrelado todo, vamos que lo he borrado

--- src/es/cursojava/excepciones/ejercicios/ejercicio1/Ejecutor1.java
+++ src/es/cursojava/excepciones/ejercicios/ejercicio1/Ejecutor1.java
@@ -1,70 +1,8 @@
 package es.cursojava.excepciones.ejercicios.ejercicio1;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import es.cursojava.oo.Alumno;
-import es.cursojava.utils.Utilidades;
-import sun.jvm.hotspot.gc.parallel.PSYoungGen;
-
 public class Ejecutor1 {
 
-	// metodos que tengo
-
-	// numero aleatorio
-
-	// crear cienco alumnos
-
-	// AlumnoException nombre = new AlumnoException(Utilidades.numeroAleatorio(0,
-	// 80));
-
-	/**
-	 * crear un lista de 5 alumnos
-	 */
-
-	// metodo crear alumno
-	public static List<AlumnoException> creaAlumnoException() {
-		List<AlumnoException> listaAlumnos = new ArrayList<AlumnoException>();
-		String nombre = Utilidades.NombreAleatorio();
-		return listaAlumnos;
-
-//			AlumnoException nombre = new AlumnoException(Utilidades.pideDatoNumerico("dime la edad"));
-//			AlumnoException Utilidades.NombreAleatorio() = new AlumnoException(Utilidades.pideDatoNumerico("dime la edad"));
-
-	}
-
-	/**
-	 * recorrer el un bucle de 5 creando el alumno y dando los datos y añade crear
-	 * metodo crear alumno llamar
-	 */
-	/**
-	 * pidecadena crear metodo apara ingresar datos alumno
-	 * 
-	 * @throws NotaInvalidaExecption
-	 * @throws IllegalAccessException
-	 * 
-	 */
-
-<<<<<<< HEAD
 	public static void main(String[] args) {
-		String nombre = Utilidades.NombreAleatorio();
-System.out.println(nombre);
-}
-=======
-	public static void main(String[] args) throws IllegalAccessException, NotaInvalidaExecption {
-		// int numeroAleatorio = Utilidades.numeroAleatorio(100);
-		// System.out.println(numeroAleatorio);
-		List<AlumnoException> pollines = new ArrayList<>();
-		// String nombre = Utilidades.NombreAleatorio();
-// crear 5 alumnos
-		AlumnoException nombre3 = new AlumnoException(35);
-		AlumnoException nombre2 = new AlumnoException(25);
-		pollines.add(nombre3);
-		pollines.add(nombre2);
-		pollines.toString();
 
 	}
->>>>>>> branch 'main' of https://github.com/dmora33/CursoJavadmora33.git
-
 }
