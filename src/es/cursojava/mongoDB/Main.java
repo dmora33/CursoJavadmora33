554037422ea7ea0607140a7afec9927df4260bd8
Isabel Eclipse <Tardes@GET-09>
2025-12-10 17:02:07 +0100

    mongolbd jijijji

---
+++ src/es/cursojava/mongoDB/Main.java
@@ -0,0 +1,30 @@
+package es.cursojava.mongoDB;
+
+import com.mongodb.client.*;
+import org.bson.Document;
+
+public class Main {
+	public static void main(String[] args) {
+
+		// *** Pega aquí tu URI de Atlas ***
+		String uri = "mongodb+srv://DaniMongodb:jYnJrpE0Zex4TVKc@cluster0.tmnrdrf.mongodb.net/?appName=Cluster0";
+
+		// Crear cliente
+		try (MongoClient mongoClient = MongoClients.create(uri)) {
+
+			// Acceder a una base de datos
+			MongoDatabase database = mongoClient.getDatabase("getafemongodb");
+
+			// Acceder a una colección
+			MongoCollection<Document> collection = database.getCollection("usuario");
+
+			// Leer un documento
+			Document doc = collection.find().first();
+			System.out.println("Primer documento: " + doc);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
